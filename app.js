d12014c42cde933325d8050d3f4f380a2ef4009c
Upasana03 <upasanachoudhury2000@gmail.com>
2022-02-13 03:10:23 +0530

    new change

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://upasana:940Uc0523@cluster0.vamok.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://upasana:940Uc0523@cluster0.vamok.mongodb.net/todolistDB?retryWrites=true&w=majority", {useNewUrlParser: true});
 
 const itemsSchema = {
     name: String
@@ -139,11 +139,11 @@ app.get("/about", function(req,res){
     res.render("about");
 });
 
-/*let port = process.env.PORT;
+let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-*/
-app.listen(process.env.PORT || 3000, function(){
+
+app.listen(port, function(){
     console.log("Server has started successfully!");
 });
